ab3148dd0a5cd49cfaeaa32ba096da343a9bc4b0
Zekarias Tesfaye Abera <zachabera1999@gmail.com>
2023-09-20 15:44:02 +0300

    air

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,19 +1,24 @@
-tarts a Flash Web Application HBNB"""
+#!/usr/bin/python3
+""" Write a script that starts a Flask web application:
+Your web application must be listening on 0.0.0.0, port 5000
+"""
+
 from flask import Flask
-app = Flask(__name__)
+
+app = Flask("__name__")
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """ Prints a Message when / is called """
-    return 'Hello HBNB!'
+def hello():
+    """Return a given string"""
+    return ("Hello HBNB!")
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """ Prints a Message when /hbnb is called """
-    return 'HBNB'
+    """Returns a given string"""
+    return ("HBNB")
+
 
 if __name__ == "__main__":
-    """ Main Function """
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=None)
